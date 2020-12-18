a33e32ad33f7043d2fec0f7095f0285bc6c71840
Kochkin Vladislav <ltrickfaim@gmail.com>
2020-12-18 10:07:45 +0600

    Подправил pre save

--- backend/models/User.js
+++ backend/models/User.js
@@ -31,18 +31,21 @@ const UserSchema = new mongoose.Schema({
         type: String,
     },
     facebookId: {
-        type: String,
-        required: true
+        type: String
     }
 }, {
     versionKey: false
 });
 
 UserSchema.pre('save', async function(next) {
-    if(this.isModified('password')) return next();
-    const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
+    if(!this.isModified('password')) return next();
+    try {
+        const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (e) {
+        return next(e);
+    }
 });
 
 UserSchema.set('toJSON', {
