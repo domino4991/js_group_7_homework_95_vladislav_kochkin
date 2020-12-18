0a2ed601c0be2be8c269959013e8013334677fa5
Kochkin Vladislav <ltrickfaim@gmail.com>
2020-12-18 10:07:20 +0600

    Добавил роут для оценок

--- backend/routes/cocktails.js
+++ backend/routes/cocktails.js
@@ -22,8 +22,20 @@ router.get('/', async (req, res) => {
     try {
         const cocktails = await Cocktail
             .find()
-            .populate('user', 'displayName role -_id');
+            .populate('user', 'displayName username role -_id');
         if(cocktails.length === 0) return res.status(404).send({error: 'Нет ни одного рецепта коктеля'});
+        return res.send(cocktails);
+    } catch (e) {
+        return res.status(500).send({error: 'Eternal Server Error'});
+    }
+});
+
+router.get('/users', [auth], async (req, res) => {
+    try {
+        const cocktails = await Cocktail
+            .find({user: req.user._id})
+            .populate('user', 'displayName username role -_id');
+        if(cocktails.length === 0) return res.status(404).send({error: 'У вас нет ни одного добавленного коктейля'});
         return res.send(cocktails);
     } catch (e) {
         return res.status(500).send({error: 'Eternal Server Error'});
@@ -56,12 +68,33 @@ router.post('/', [auth, upload.single('cocktailImage')], async (req, res) => {
     }
 });
 
+router.put('/:id/score', auth, async (req, res) => {
+    try {
+        const cocktail = await Cocktail
+            .findById(req.params.id);
+        if(!cocktail) return res.status(404)
+            .send({error: 'Коктейль не найден'});
+        const scoreIndex = cocktail.rating
+            .findIndex(score => score.user === req.user.username);
+        if(scoreIndex === -1) {
+            cocktail.rating.push(req.body)
+        } else {
+            cocktail.rating[scoreIndex].score = req.body.score ?
+                req.body.score : cocktail.rating[scoreIndex].score;
+        }
+        await Cocktail.findOneAndUpdate({_id: req.params.id}, cocktail);
+        return res.send({message: 'Спасибо за Ваш голос!'});
+    } catch (e) {
+        return res.status(500).send({error: 'Eternal Server Error'});
+    }
+});
+
 router.put('/:id/publish', [auth, permit('admin')], async (req, res) => {
     try {
         const cocktail = await Cocktail.findById(req.params.id);
         cocktail.isPublished = !cocktail.isPublished;
         await cocktail.save();
-        return res.send({message: `Коктель ${cocktail.cocktailName} опубликован`});
+        return res.send({message: `Коктель ${cocktail.cocktailName} ${cocktail.isPublished ? 'опубликован' : 'cнят с публикации'}`});
     } catch (e) {
         return res.status(422).send(e);
     }
